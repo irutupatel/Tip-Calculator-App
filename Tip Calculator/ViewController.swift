f0c3299640bddaab633d24abe8522d507544cfb1
irutupatel <rpate347@illinois.edu>
2019-09-21 22:33:18 -0700

    Initial Commit

---
+++ Tip Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip Calculator
+//
+//  Created by Rutu Patel on 9/21/19.
+//  Copyright © 2019 Rutu Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
